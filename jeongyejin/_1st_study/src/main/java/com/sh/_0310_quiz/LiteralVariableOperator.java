5d4a3ec0b0f385fd7a7524f3b4683635501c1feb
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-03-10 08:51:40 +0900

    Feat: jeongyejin 디렉토리 추가

---
+++ jeongyejin/_1st_study/src/main/java/com/sh/_0310_quiz/LiteralVariableOperator.java
@@ -0,0 +1,22 @@
+package com.sh._0310_quiz;
+
+public class LiteralVariableOperator {
+    public static void main(String[] args) {
+        // 1번
+        /*
+        char x = 'E';
+        System.out.println(x > 97 ? ++x : x++); // 1-1번 답 :
+        System.out.println(x); // 1-2번 답 :
+        System.out.println();
+        */
+
+        // 2번
+        /*
+        double y = 22;
+        final int z = 3;
+        int result;
+        double result2;
+        System.out.println(y/z == 7 ? z++ : z % 3);
+        */
+    }
+}
